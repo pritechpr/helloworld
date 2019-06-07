7572102ffe009122d5ffecebfa953a1f1d79d0fb
Angel Berrios Davila <angel.berrios@pritechpr.com>
2019-06-07 08:54:25 -0400

    Serving node js configuration in image

---
+++ helloworld.webapp/src/config.js
@@ -0,0 +1,6 @@
+const settings = {
+  API_URL: window.env.API_URL,
+  GA_PROPERTY: window.env.GA_PROPERTY,
+};
+
+export default settings;
---
+++ helloworld.webapp/src/public/config.js
@@ -0,0 +1,2 @@
+window.env.API_URL=http://helloworld-itrellis.you2service.com/api/v1/
+window.env.GA_PROPERTY=TRACK_ME
