297699d17ff3da3e8eecff56ea44d9279e8d066b
Angel Berrios Davila <angel.berrios@pritechpr.com>
2019-06-10 16:31:28 -0400

    Looking for hostname react value

--- helloworld.webapp/src/config.js
+++ helloworld.webapp/src/config.js
@@ -1,8 +1,8 @@
 const settings = {
-  PUBLIC_URL: window.env.PUBLIC_URL,
+//  PUBLIC_URL: window.env.PUBLIC_URL,
   HOST: window.env.HOST,
-  API_URL: window.env.API_URL,
-  GA_PROPERTY: window.env.GA_PROPERTY,
+//  API_URL: window.env.API_URL,
+//  GA_PROPERTY: window.env.GA_PROPERTY,
 };
 
 export default settings;
