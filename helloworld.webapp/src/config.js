fcaa0b33da604404d6427e8724d50a5cead0f98f
Angel Berrios Davila <angel.berrios@pritechpr.com>
2019-06-10 17:47:01 -0400

    Looking for the wildcard host

--- helloworld.webapp/src/config.js
+++ helloworld.webapp/src/config.js
@@ -1,6 +1,6 @@
 const settings = {
 //  PUBLIC_URL: window.env.PUBLIC_URL,
-//  HOST: window.env.HOST,
+  HOST: window.env.HOST,
 //  API_URL: window.env.API_URL,
 //  GA_PROPERTY: window.env.GA_PROPERTY,
 };
