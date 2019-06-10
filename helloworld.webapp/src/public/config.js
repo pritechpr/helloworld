f68a5e7a2af448324ceca6fc6393f6ec315c74fd
Angel Berrios Davila <angel.berrios@pritechpr.com>
2019-06-10 18:56:42 -0400

    Looking for the wildcard host

--- helloworld.webapp/src/public/config.js
+++ helloworld.webapp/src/public/config.js
@@ -1,4 +1,4 @@
-window.env.HOST=*
-//window.env.API_URL=http://helloworld-itrellis.you2service.com/api/v1/
-//window.env.PUBLIC_URL=http://helloworld-itrellis.you2service.com
+window.env.HOST=0.0.0.0
+window.env.API_URL=http://helloworld-itrellis.you2service.com:3000
+window.env.PUBLIC_URL=http://helloworld-itrellis.you2service.com:3000
 //window.env.GA_PROPERTY=TRACK_ME
